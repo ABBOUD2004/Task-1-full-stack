f93358d6011ee8002e940a58a2f3fb32ddc2c29e
abdallah mohamed elsayed <169095889+ABBOUD2004@users.noreply.github.com>
2025-07-14 13:14:52 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP</title>
+  <link rel="stylesheet" href="./bootstrap-5.3.7-dist/bootstrap-5.3.7-dist/css/bootstrap.min.css" />
+
+</head>
+<body>
+    <body class="d-flex justify-content-center align-items-center vh-100 bg-black">
+  <div class="form bg-white p-5 " style="width: 400px;">
+    <form>
+      <h2 class="mb-4 text-center">User Information</h2>
+
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" required minlength="6" />
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">age</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" required minlength="6" />
+      </div>
+
+<div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">city</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" required minlength="6" />
+      </div>
+
+
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" required />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+
+      
+
+      <button type="submit" class="btn btn-primary w-100">Submit</button>
+    </form>
+  </div>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>deco</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex justify-content-center align-items-center vh-100 bg-success bg-gradient">
+  <div class="card p-4 shadow-lg" style="width: 400px;">
+    <h4 class="text-center mb-3">User Profile</h4>
+    <div class="alert alert-success text-center p-2 py-1">
+      Welcome, <strong>abdalllah</strong>!
+    </div>
+    <div class="border rounded p-3 bg-light">
+      <h6 class="mb-3">User Information</h6>
+      <p><strong>Full Name:</strong> abdallah mohamed</p>
+      <p><strong>Email:</strong> abdallahelsayed@gmail.com</p>
+      <p><strong>Age:</strong> 21</p>
+      <p><strong>City:</strong> welcome</p>
+    </div>
+    <div class="text-center mt-3">
+      <a href="form.html" class="btn btn-primary">Back to Form</a>
+    </div>
+  </div>
+</body>
+</html>
